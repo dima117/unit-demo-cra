ad922afea004b6097f476d04085e7209136410cc
Rubicones <dmitriypopov1543@gmail.com>
2023-07-11 18:51:11 +0400

    semantic-release added

---
+++ release.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    branches: ["master"],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/changelog",
+            {
+                changelogFile: "CHANGELOG.md",
+            },
+        ],
+        "@semantic-release/npm",
+        [
+            "@semantic-release/github",
+            {
+                assets: ["build/*.js"],
+            },
+        ],
+        [
+            "@semantic-release/git",
+            {
+                assets: ["CHANGELOG.md"],
+                message:
+                    "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+            },
+        ],
+    ],
+};
