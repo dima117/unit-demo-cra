a80edaae88c3c713b77c0fce5067eb4e18a0bbf5
maxim <fedaraumaxim@gmail.com>
2023-07-11 18:35:02 +0300

    feat: add issue body

--- .github/actions/createRelease/dist/index.js
+++ .github/actions/createRelease/dist/index.js
@@ -9803,20 +9803,31 @@ const main = async () => {
 
         const ref = core.getInput("ref", {required: true});
         const tag_name = ref.split("/")[2];
+
+        const actor = core.getInput("actor", {required: true});
         
         const octokit = github.getOctokit(token);
-        await octokit.rest.repos.createRelease({
+        const {data} = await octokit.rest.repos.createRelease({
             owner, 
             repo,
             tag_name,
             generate_release_notes: true,
         });
 
+        const issueBody = `<h1 align="center">RELEASE DATA</h1>
+
+- Author: ${actor}
+        
+- Date: ${data.created_at}
+        
+- Version: ${tag_name}`
+
         await octokit.rest.issues.create({
             owner,
             repo,
             title: `RELEASE - ${tag_name}`,
             labels: ["RELEASE"],
+            body: issueBody,
         })
     } catch (error) {
         core.setFailed(error.message);
