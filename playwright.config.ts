0c399610a666eb7407db9e8961eec34c85fe6d01
Yaroslav Urakov <98356679+Yaros1ove@users.noreply.github.com>
2023-07-12 02:46:20 +0300

    test: test

--- playwright.config.ts
+++ playwright.config.ts
@@ -111,6 +111,7 @@ const config: PlaywrightTestConfig = {
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run start",
+    reuseExistingServer: true,
     port: 3000,
   },
 };
