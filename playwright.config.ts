ad0c08946fe6af88538cb4f330b309e077a5dfd4
EvgenyTomson <tomilin.evgeny@gmail.com>
2023-07-11 13:39:38 +0300

    fix: cut package.json show-report

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ const config: PlaywrightTestConfig = {
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   /* reporter: process.env.CI ? [ ['html', { open: 'never' }] ] : "html", */
-  reporter: [ ['html', { open: 'never' }] ],
+  reporter: process.env.CI ? [ ['html', { open: 'never' }] ] : "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
