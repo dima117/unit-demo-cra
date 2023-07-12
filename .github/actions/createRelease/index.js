3e809fb0a522514069926cfcb115ccf31abf166c
maxim <fedaraumaxim@gmail.com>
2023-07-12 12:03:24 +0300

    feat: release branch handling

--- .github/actions/createRelease/index.js
+++ .github/actions/createRelease/index.js
@@ -10,8 +10,20 @@ const main = async () => {
         const actor = core.getInput("actor", {required: true});
         const ref = core.getInput("ref", {required: true});
 
-        const tag_name = ref.split("/")[2];
+        const tag_name = ref.split("/")[ref.split("/").length - 1];
         const octokit = github.getOctokit(token);
+
+        // создаем релизную ветку, если ее не существует
+        const {data: branches} = await octokit.rest.repos.listBranches({owner, repo});
+        let branchExists = false;
+        branches.forEach((branch) => {
+            if (branch.name === `release/${tag_name}`) branchExists = true;
+        })
+        const releaseBranchName = `release/${tag_name}`
+        if (!branchExists) {
+            await exec.exec("git", ["checkout", "-b", releaseBranchName])
+            await exec.exec("git", ["push", "origin", releaseBranchName])
+        }
 
         // создаем релиз, если его не существует
         const {data: releases} = await octokit.rest.repos.listReleases({owner, repo});
@@ -59,12 +71,12 @@ const main = async () => {
             },
         };
         if (tags.length === 1) {
-            await exec.exec('git', ['log', "--pretty=oneline"] , commitsOptions);
+            await exec.exec('git', ['log', "--pretty=oneline", releaseBranchName] , commitsOptions);
         } else {
             const secondTagName = tags[tags.indexOf(tag_name) - 1].trim();
             // можем идти от HEAD так как 1) в случае push-a нового тега - коммит с новым тегом и будет HEAD 2) в случае ветки release/* - HEAD может также содержать hotfixes
             // + если бы у нас была ветка release/*, то мы бы делали git log от нее, а не от главной ветки
-            await exec.exec('git', ['log', "--pretty=oneline", `HEAD...${secondTagName}`] , commitsOptions);
+            await exec.exec('git', ['log', "--pretty=oneline", releaseBranchName, `HEAD...${secondTagName}`] , commitsOptions);
         }
 
         // фильтруем коммиты по заголовку
@@ -119,14 +131,25 @@ ${fullChangelog}
 </details>`
         }
 
-        // создаем issue
-        await octokit.rest.issues.create({
-            owner,
-            repo,
-            title: `RELEASE - ${tag_name}`,
-            labels: ["RELEASE"],
-            body: issueBody,
+        // создаем issue, если его не сущесвует
+        const {data: issues} = await octokit.rest.issues.listForRepo({owner, repo, labels: ["RELEASE"]});
+        
+        let issueNumber;
+        issues.forEach((issue) => {
+            if (issue.title === `RELEASE - ${tag_name}`) issueNumber = issue.number;
         })
+
+        if (issueNumber === undefined) {
+            await octokit.rest.issues.create({
+                owner,
+                repo,
+                title: `RELEASE - ${tag_name}`,
+                labels: ["RELEASE"],
+                body: issueBody,
+            })
+        } else {
+            await octokit.rest.issues.update({owner, repo, issue_number: issueNumber, body: issueBody})
+        }
     } catch (error) {
         core.setFailed(error.message);
     }
