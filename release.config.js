a6e6c3b89a586592f81ef77113f941d7b21eb83b
Rubicones <dmitriypopov1543@gmail.com>
2023-07-12 15:08:41 +0400

    feat: work on 'release' issues

--- release.config.js
+++ release.config.js
@@ -20,11 +20,15 @@ module.exports = {
             "@semantic-release/git",
             {
                 assets: ["CHANGELOG.md"],
-                tagFormat: "v${nextRelease.version}",
                 message:
                     "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-
+                tagFormat: 'v${nextRelease.version}',
             },
         ],
+        ["@semantic-release/github", {
+            assignees: ["Rubicones"],
+            labels: ["RELEASE"],
+            createComment: true
+          }]
     ],
 };
