9c41b837a30c9dc4036fbede751bf0075ecd6829
Rubicones <dmitriypopov1543@gmail.com>
2023-07-12 19:12:43 +0400

    test: issue customize

--- create-release-issue.sh
+++ create-release-issue.sh
@@ -1,12 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
-REPO=Rubicones/hw-infra
-TOKEN=$1
 VERSION=$2
-NOTES=$3
-curl -X POST -H "Authorization: token $TOKEN" -d '{
-  "title": "Release v'"$VERSION"'",
-  "body": "'"${NOTES//\"/\\\"}"'",
-  "labels": ["RELEASE"]
-}' "https://api.github.com/repos/$REPO/issues"
+CHANGELOG=$3
+GH_TOKEN=$1
+TAG_NAME=$(git describe --tags --abbrev=0)
+TAG_DATE=$(git log -1 --pretty=format:"%ai" $TAG_NAME)
+AUTHOR=$(git show $TAG_NAME --pretty="format:%an" --no-patch)
 
+create_issue_payload() {
+  cat <<EOF
+  {
+    "title": "Release $VERSION",
+    "labels": ["RELEASE"],
+    "body": "**Release version:** $VERSION \n**Tag:** $TAG_NAME \n**Tagged by:** $AUTHOR \n**Date:** $TAG_DATE \n\n$CHANGELOG"
+  }
+EOF
+}
+
+curl \
+  -X POST \
+  -H "Authorization: token $GH_TOKEN" \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/$GITHUB_REPOSITORY/issues \
+  -d "$(create_issue_payload)"
