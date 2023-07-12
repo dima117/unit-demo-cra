797f0370ea1aa1231cec53eeae138fe111390834
Ilya Sulkhanov <masta.pxty@gmail.com>
2023-07-12 20:35:56 +0700

    feat: eslint prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  importOrder: ['^@common/(.*)$', '^@/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  semi: true
+};
