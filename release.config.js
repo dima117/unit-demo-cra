6aff6eace58558650c154581073141d6da49e5e1
Rubicones <dmitriypopov1543@gmail.com>
2023-07-12 11:46:06 +0400

    fix: semantic release now puts 'v' at the start of version

--- release.config.js
+++ release.config.js
@@ -20,8 +20,10 @@ module.exports = {
             "@semantic-release/git",
             {
                 assets: ["CHANGELOG.md"],
+                tagFormat: "v${nextRelease.version}",
                 message:
                     "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+
             },
         ],
     ],
