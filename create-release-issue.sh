c70f69332a7551e2ad3f3229d3dc4d5a22b57741
Rubicones <dmitriypopov1543@gmail.com>
2023-07-13 16:48:26 +0400

    test: testing issue creating script

--- create-release-issue.sh
+++ create-release-issue.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-VERSION=$2
 CHANGELOG=$3
 GH_TOKEN=$1
-TAG_NAME=$(git describe --tags --abbrev=0)
+VERSION=$(git describe --tags --abbrev=0)
 TAG_DATE=$(git log -1 --pretty=format:"%ai" $TAG_NAME)
 AUTHOR=$(git show $TAG_NAME --pretty="format:%an" --no-patch)
 
@@ -12,7 +11,7 @@ create_issue_payload() {
   {
     "title": "Release $VERSION",
     "labels": ["RELEASE"],
-    "body": "**Release version:** $VERSION \n**Tag:** $TAG_NAME \n**Tagged by:** $AUTHOR \n**Date:** $TAG_DATE \n\n$CHANGELOG"
+    "body": "**Release version:** $VERSION \n**Tagged by:** $AUTHOR \n**Date:** $TAG_DATE \n\n$CHANGELOG"
   }
 EOF
 }
@@ -23,4 +22,3 @@ curl \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/$GITHUB_REPOSITORY/issues \
   -d "$(create_issue_payload)"
-  
