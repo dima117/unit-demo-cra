2453df11fb9b39f4250d91587fe309e41535c167
Alexander Palanevich <48152200+Sisha3342@users.noreply.github.com>
2023-07-13 15:07:51 +0200

    feat(pages): update about page description (#3)

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,12 +3,12 @@ import { Helmet } from 'react-helmet';
 
 export const About: FC = () => {
   return (
-    <>
-      <Helmet>
-        <title>About page</title>
-      </Helmet>
-      <h1 data-testid="page-title">About</h1>
-      <p>This is an example application.</p>
-    </>
+		<>
+			<Helmet>
+				<title>About page</title>
+			</Helmet>
+			<h1 data-testid="page-title">About</h1>
+			<p>An example application.</p>
+		</>
   );
 };
