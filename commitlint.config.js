1522d6db5db6022845368a85ac286acba7570cb8
kot <talalaev.misha@gmail.com>
2023-07-13 17:45:13 +0400

    ci(commitlint): added button as a type of commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,4 +2,9 @@ module.exports = {
   extends: [
     "@commitlint/config-conventional"
   ],
+  rules: {
+    "type-enum": [2, "always", [
+      "feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"
+    ]],
+  }
 }
