4abe5344104131911b1fa4fa5538a7cb3d491550
Alexander Palanevich <48152200+Sisha3342@users.noreply.github.com>
2023-07-13 15:11:01 +0200

    feat(pages): update home page description (#4)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,13 +5,13 @@ import { TodoList } from '../components/TodoList';
 
 export const Home: FC = () => {
   return (
-    <>
-      <Helmet>
-        <title>Home page</title>
-      </Helmet>
-      <h1 data-testid="page-title">Home</h1>
-      <p>This is the list.</p>
-      <TodoList />
-    </>
+		<>
+			<Helmet>
+				<title>Home page</title>
+			</Helmet>
+			<h1 data-testid="page-title">Home</h1>
+			<p>Todo list.</p>
+			<TodoList />
+		</>
   );
 };
