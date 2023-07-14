f6a648579a16711c22b3e86cb7452b6b3c43e079
Memesaurus <Greggrr1702@gmail.com>
2023-07-14 21:39:44 +0300

    fix: hotfix two

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,7 +7,6 @@ export const About: FC = () => {
       <Helmet>
         <title>About page</title>
       </Helmet>
-      
       <h1 data-testid="page-title">About</h1>
       <p>This is an example application.</p>
     </>
