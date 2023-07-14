8ec05732b4eeb7a7e5b4ca1e0a0e503c668c0e93
Rubicones <dmitriypopov1543@gmail.com>
2023-07-14 17:09:52 +0400

    feat: release tests added

--- create-release-issue.sh
+++ create-release-issue.sh
@@ -4,13 +4,14 @@ GH_TOKEN=$1
 VERSION=$(git describe --tags --abbrev=0)
 TAG_DATE=$(git log -1 --pretty=format:"%ai" $VERSION)
 AUTHOR=$(git show $VERSION --pretty="format:%an" --no-patch)
+RUN_ID=$2
 
 create_issue_payload() {
   cat <<EOF
   {
     "title": "Release $VERSION",
     "labels": ["RELEASE"],
-    "body": "**Release version:** $VERSION \n**Tagged by:** $AUTHOR \n**Date:** $TAG_DATE \n"
+    "body": "**Release version:** $VERSION \n**Tagged by:** $AUTHOR \n**Date:** $TAG_DATE \n\n[Link to checks results](https://github.com/$GITHUB_REPOSITORY/actions/runs/$RUN_ID)"
   }
 EOF
 }
