a190cc54d2392576a5837fffb6377ed6ce4cd08c
Memesaurus <Greggrr1702@gmail.com>
2023-07-14 21:39:18 +0300

    test: hotfix test

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,6 +7,7 @@ export const About: FC = () => {
       <Helmet>
         <title>About page</title>
       </Helmet>
+      
       <h1 data-testid="page-title">About</h1>
       <p>This is an example application.</p>
     </>
