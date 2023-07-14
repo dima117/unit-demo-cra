4abee03f29ca9e241ff970cebc937f4a349c7f5e
Vlad Koncha <vla.koncha@gmail.com>
2023-07-14 19:20:03 +0700

    feat: adds new title (#39)

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,7 +5,7 @@ export const About: FC = () => {
   return (
     <>
       <Helmet>
-        <title>About page</title>
+        <title>New About page</title>
       </Helmet>
       <h1 data-testid="page-title">About</h1>
       <p>This is an example application.</p>
