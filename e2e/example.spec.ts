c2f3db051ee62bda2cd8787924dd6a3ba82a3a40
Eugene Lositsky <evgenylositskyitmo@gmail.com>
2023-07-15 19:56:09 +0400

    Fixed tests

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/test";
 test('по адресу /about открывается страница "о проекте"', async ({ page }) => {
   await page.goto("/about");
 
-  await expect(page.getByTestId("page-title")).toHaveText("Про нас");
+  await expect(page.getByTestId("page-title")).toHaveText("About");
 });
 
 test("если добавить элемент, он появляется в списке", async ({ page }) => {
