2c11320c9e60c0d9041512528c2241beb47e3c2c
slry <v.rybalchenko@innopolis.university>
2023-07-16 03:45:51 +0300

    chore: add script logs

--- .github/scripts/generate_report.sh
+++ .github/scripts/generate_report.sh
@@ -5,10 +5,20 @@
 latest_tag=$(git describe --tags --abbrev=0)
 previous_tag=$(git describe --tags --abbrev=0 $latest_tag^)
 
+echo "Latest tag: $latest_tag"
+echo "Previous tag: $previous_tag"
+
+# Get author and date of latest tag
+
 author=$(git log -1 $latest_tag --pretty="%an")
 date=$(git log -1 $latest_tag --pretty="%ad")
 
-changelog=$(git log $previous_tag..$latest_tag --oneline)
+echo "Author: $author"
+echo "Date: $date"
+
+changelog=$(git log $previous_tag...$latest_tag --oneline)
+
+echo "Changelog: $changelog"
 
 # Send to github env
 echo "TAG=$latest_tag" >> $GITHUB_ENV
