a8e8ba04e6e133d51dc22668b4a3a54c6aa82490
Ilya Sulkhanov <masta.pxty@gmail.com>
2023-07-16 23:18:05 +0700

    feat: add test comp 1

--- src/testing.tsx
+++ src/testing.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  return <h1>Test componentddddddddddddddddddddddddddddddddddd</h1>;
+  return <h1>Test cwwwdawdomponentddddddddddddddddddddddddddddddddddd</h1>;
 };
 
 export default App;
