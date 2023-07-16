07aa05001a161f5dd39c282808f6449974ecb9a0
Rubicones <dmitriypopov1543@gmail.com>
2023-07-16 15:16:15 +0400

    feat: added 'released by' line in Issue

--- create-release-issue.sh
+++ create-release-issue.sh
@@ -3,7 +3,7 @@
 GH_TOKEN=$1
 VERSION=$(git describe --tags --abbrev=0)
 TAG_DATE=$(git log -1 --pretty=format:"%ai" $VERSION)
-AUTHOR=$(git show $VERSION --pretty="format:%an" --no-patch)
+RELEASED_BY=$(git log -1 --pretty=format:"%an")
 RUN_ID=$2
 CHANGES=$(awk -v RS='\n\n' -v version="$VERSION" '$0 ~ version' CHANGELOG.md)
 
@@ -12,7 +12,7 @@ create_issue_payload() {
   {
     "title": "Release $VERSION",
     "labels": ["RELEASE"],
-    "body": "**Release version:** $VERSION \n**Tagged by:** $AUTHOR \n**Date:** $TAG_DATE \n\n[Check tests results link](https://github.com/$GITHUB_REPOSITORY/actions/runs/$RUN_ID) \n\n**Changelog:** \n$CHANGES"
+    "body": "**Release version:** $VERSION \n**Released by:** $RELEASED_BY \n**Date:** $TAG_DATE \n\n[Check tests results link](https://github.com/$GITHUB_REPOSITORY/actions/runs/$RUN_ID) \n\n**Changelog:** \n$CHANGES"
   }
 EOF
 }
