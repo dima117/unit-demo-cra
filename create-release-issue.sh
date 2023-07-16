1100b13659e6479ee64df7b492a0be32feb1e72f
Rubicones <dmitriypopov1543@gmail.com>
2023-07-16 13:02:46 +0400

    test: issue script testing

--- create-release-issue.sh
+++ create-release-issue.sh
@@ -16,6 +16,7 @@ create_issue_payload() {
 EOF
 }
 VERSION_ENCODED=$(echo $VERSION | jq -sRr @uri)
+echo $VERSION_ENCODED
 
 EXISTING_ISSUE=$(curl \
   -H "Authorization: token $GH_TOKEN" \
@@ -23,20 +24,21 @@ EXISTING_ISSUE=$(curl \
   -X GET \
   https://api.github.com/search/issues?q=repo:$GITHUB_REPOSITORY+label:RELEASE+title:"Release $VERSION_ENCODED")
 
+echo $EXISTING_ISSUE_NUMBER
 EXISTING_ISSUE_NUMBER=$(echo $EXISTING_ISSUE | jq '.items[0].number')
-
-# if [ -n "$EXISTING_ISSUE_NUMBER" ]; then
-#   curl \
-#     -X PATCH \
-#     -H "Authorization: token $GH_TOKEN" \
-#     -H "Accept: application/vnd.github.v3+json" \
-#     https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$EXISTING_ISSUE_NUMBER \
-#     -d "$(create_issue_payload)"
-# else
-#   curl \
-#     -X POST \
-#     -H "Authorization: token $GH_TOKEN" \
-#     -H "Accept: application/vnd.github.v3+json" \
-#     https://api.github.com/repos/$GITHUB_REPOSITORY/issues \
-#     -d "$(create_issue_payload)"
-# fi
+echo $EXISTING_ISSUE_NUMBER
+if [ -n "$EXISTING_ISSUE_NUMBER" ]; then
+  curl \
+    -X PATCH \
+    -H "Authorization: token $GH_TOKEN" \
+    -H "Accept: application/vnd.github.v3+json" \
+    https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$EXISTING_ISSUE_NUMBER \
+    -d "$(create_issue_payload)"
+else
+  curl \
+    -X POST \
+    -H "Authorization: token $GH_TOKEN" \
+    -H "Accept: application/vnd.github.v3+json" \
+    https://api.github.com/repos/$GITHUB_REPOSITORY/issues \
+    -d "$(create_issue_payload)"
+fi
