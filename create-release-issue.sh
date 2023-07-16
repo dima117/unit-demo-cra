800207cb4ecd5fbf3afd73db01fb65b92824dea4
Rubicones <dmitriypopov1543@gmail.com>
2023-07-16 20:41:30 +0400

    test: closing issue after successful deploy test

--- create-release-issue.sh
+++ create-release-issue.sh
@@ -30,30 +30,18 @@ get_existing_issue_number() {
        -H "Accept: application/vnd.github.v3+json" \
        "https://api.github.com/search/issues?q=repo:$GITHUB_REPOSITORY+label:RELEASE+in:title+$(echo "Release $VERSION" | sed 's/ /+/g')" | jq '.items[0].number'
 }
-echo "before"
 
 issue_number=$(get_existing_issue_number)
-echo "after"
 
-# if [ "$issue_number" != "null" ]; then
-#   echo "Issue already exists, adding a comment to existing issue #$issue_number"
-#   curl -X POST \
-#        -H "Authorization: token $GH_TOKEN" \
-#        -H "Accept: application/vnd.github.v3+json" \
-#        "https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$issue_number/comments" \
-#        -d "$(create_comment_payload)"
-# else
-#     echo "No existing issue found, creating a new one"
-#     response=$(curl -sS \
-#         -X POST \
-#         -H "Authorization: token $GH_TOKEN" \
-#         -H "Accept: application/vnd.github.v3+json" \
-#         https://api.github.com/repos/$GITHUB_REPOSITORY/issues \
-#         -d "$(create_issue_payload)")
-#     echo $response
-#     issue_number=$(echo "$response" | jq '.number')
-# fi
-echo "No existing issue found, creating a new one"
+if [ "$issue_number" != "null" ]; then
+  echo "Issue already exists, adding a comment to existing issue #$issue_number"
+  curl -X POST \
+       -H "Authorization: token $GH_TOKEN" \
+       -H "Accept: application/vnd.github.v3+json" \
+       "https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$issue_number/comments" \
+       -d "$(create_comment_payload)"
+else
+    echo "No existing issue found, creating a new one"
     response=$(curl -sS \
         -X POST \
         -H "Authorization: token $GH_TOKEN" \
@@ -62,6 +50,7 @@ echo "No existing issue found, creating a new one"
         -d "$(create_issue_payload)")
     echo $response
     issue_number=$(echo "$response" | jq '.number')
+fi
 echo $issue_number
 echo "$issue_number" > issue_number.txt
 
