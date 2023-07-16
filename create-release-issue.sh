e87a0b733f6c8917f31290c9eebf3aa7777b9972
Rubicones <dmitriypopov1543@gmail.com>
2023-07-16 15:00:54 +0400

    test: issue script changelog added

--- create-release-issue.sh
+++ create-release-issue.sh
@@ -5,13 +5,14 @@ VERSION=$(git describe --tags --abbrev=0)
 TAG_DATE=$(git log -1 --pretty=format:"%ai" $VERSION)
 AUTHOR=$(git show $VERSION --pretty="format:%an" --no-patch)
 RUN_ID=$2
+CHANGES=$(awk -v RS='\n\n' -v version="$VERSION" '$0 ~ version' CHANGELOG.md)
 
 create_issue_payload() {
   cat <<EOF
   {
     "title": "Release $VERSION",
     "labels": ["RELEASE"],
-    "body": "**Release version:** $VERSION \n**Tagged by:** $AUTHOR \n**Date:** $TAG_DATE \n\n[Check tests results link](https://github.com/$GITHUB_REPOSITORY/actions/runs/$RUN_ID)"
+    "body": "**Release version:** $VERSION \n**Tagged by:** $AUTHOR \n**Date:** $TAG_DATE \n\n[Check tests results link](https://github.com/$GITHUB_REPOSITORY/actions/runs/$RUN_ID) \n\n**Changelog:** \n$CHANGES"
   }
 EOF
 }
