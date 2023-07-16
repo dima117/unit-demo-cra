7c2569d63f63f971cc3f8898600646393961d7b1
Rubicones <dmitriypopov1543@gmail.com>
2023-07-16 12:51:47 +0400

    test: issue script testing

--- create-release-issue.sh
+++ create-release-issue.sh
@@ -15,28 +15,4 @@ create_issue_payload() {
   }
 EOF
 }
-VERSION_ENCODED=$(echo $VERSION | jq -sRr @uri)
 
-EXISTING_ISSUE=$(curl \
-  -H "Authorization: token $GH_TOKEN" \
-  -H "Accept: application/vnd.github.v3+json" \
-  -X GET \
-  https://api.github.com/search/issues?q=repo:$GITHUB_REPOSITORY+label:RELEASE+title:"Release $VERSION_ENCODED")
-
-EXISTING_ISSUE_NUMBER=$(echo $EXISTING_ISSUE | jq '.items[0].number')
-
-if [ -n "$EXISTING_ISSUE_NUMBER" ]; then
-  curl \
-    -X PATCH \
-    -H "Authorization: token $GH_TOKEN" \
-    -H "Accept: application/vnd.github.v3+json" \
-    https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$EXISTING_ISSUE_NUMBER \
-    -d "$(create_issue_payload)"
-else
-  curl \
-    -X POST \
-    -H "Authorization: token $GH_TOKEN" \
-    -H "Accept: application/vnd.github.v3+json" \
-    https://api.github.com/repos/$GITHUB_REPOSITORY/issues \
-    -d "$(create_issue_payload)"
-fi
