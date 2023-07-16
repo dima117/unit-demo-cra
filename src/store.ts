86da75865cc1c89ec405dd2c726b3c801dc8ec62
Eugene Lositsky <evgenylositskyitmo@gmail.com>
2023-07-17 03:17:22 +0400

    feat: less tasks on start

--- src/store.ts
+++ src/store.ts
@@ -6,7 +6,7 @@ export interface ApplicationState {
   done: Record<number, boolean>;
 }
 
-const items = ["Позавтракать", "Сходить в магазин", "Покормить кота", "Новая задача", "Ещё одна новая задача", "Задача номер 6"];
+const items = ["Позавтракать", "Сходить в магазин", "Покормить кота", "Новая задача", "Ещё одна новая задача"];
 
 const DEFAULT_STATE = { items, done: {}, text: "" };
 
