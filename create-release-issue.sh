21ebeb4311cbeb98772a261229036fc2970be834
Rubicones <dmitriypopov1543@gmail.com>
2023-07-16 18:11:27 +0400

    test: testing issue creation

--- create-release-issue.sh
+++ create-release-issue.sh
@@ -17,21 +17,28 @@ create_issue_payload() {
 EOF
 }
 
-get_existing_issue_number() {
+get_existing_issue() {
   curl -H "Authorization: token $GH_TOKEN" \
        -H "Accept: application/vnd.github.v3+json" \
-       "https://api.github.com/search/issues?q=repo:$GITHUB_REPOSITORY+label:RELEASE+in:title+$(echo "Release $VERSION" | sed 's/ /+/g')" | jq '.items[0].number'
+       "https://api.github.com/search/issues?q=repo:$GITHUB_REPOSITORY+label:RELEASE+in:title+$(echo "Release $VERSION" | sed 's/ /+/g')"
 }
 
-issue_number=$(get_existing_issue_number)
+issue_response=$(get_existing_issue)
+issue_number=$(echo "$issue_response" | jq '.items[0].number')
+issue_body=$(echo "$issue_response" | jq -r '.items[0].body')
 
 if [ "$issue_number" != "null" ]; then
   echo "Issue already exists, updating existing issue #$issue_number"
+  
+  NEW_BODY="**Release version:** $VERSION \n**Released by:** $RELEASED_BY \n**Date:** $TAG_DATE \n\n[Check tests results link](https://github.com/$GITHUB_REPOSITORY/actions/runs/$RUN_ID) \n\n**Changelog:** \n$CHANGES \n\n $issue_body"
+  
+  update_issue_payload="{ \"title\": \"Release $VERSION\", \"labels\": [\"RELEASE\"], \"body\": \"$NEW_BODY\" }"
+  
   curl -X PATCH \
        -H "Authorization: token $GH_TOKEN" \
        -H "Accept: application/vnd.github.v3+json" \
        "https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$issue_number" \
-       -d "$(create_issue_payload)"
+       -d "$update_issue_payload"
 else
   echo "No existing issue found, creating a new one"
   curl -X POST \
