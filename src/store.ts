576141484fbf444996c027863f00cfc60ca2fb7f
Eugene Lositsky <evgenylositskyitmo@gmail.com>
2023-07-16 14:40:54 +0400

    fix: store deletion to test tags

--- src/store.ts
+++ src/store.ts
@@ -6,7 +6,7 @@ export interface ApplicationState {
   done: Record<number, boolean>;
 }
 
-const items = ["Позавтракать", "Сходить в магазин", "Покормить кота", "Почиллить"];
+const items = ["Позавтракать", "Сходить в магазин", "Покормить кота"];
 
 const DEFAULT_STATE = { items, done: {}, text: "" };
 
