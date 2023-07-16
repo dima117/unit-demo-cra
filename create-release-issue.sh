ae94112969339b74b8306438cdfbef58d3e16a9b
Rubicones <dmitriypopov1543@gmail.com>
2023-07-16 14:53:29 +0400

    test: issue script testing

--- create-release-issue.sh
+++ create-release-issue.sh
@@ -16,43 +16,9 @@ create_issue_payload() {
 EOF
 }
 
-ALL_ISSUES=$(curl \
-  -H "Authorization: token $GH_TOKEN" \
-  -H "Accept: application/vnd.github.v3+json" \
-  -X GET \
-  https://api.github.com/repos/$GITHUB_REPOSITORY/issues)
-
-EXISTING_ISSUE_NUMBER=""
-
-# Convert the JSON array into an array of base64 encoded strings
-ENCODED_ISSUES=$(echo "${ALL_ISSUES}" | jq -c '.[]' | awk '{print $0}' | base64)
-
-for encoded_row in ${ENCODED_ISSUES}
-do
-    # Decode the base64 encoded JSON
-    row=$(echo "${encoded_row}" | base64 --decode)
-    
-    ISSUE_TITLE=$(echo "${row}" | jq -r '.title')
-    ISSUE_NUMBER=$(echo "${row}" | jq -r '.number')
-
-    if [ "$ISSUE_TITLE" = "Release $VERSION" ]; then
-        EXISTING_ISSUE_NUMBER="$ISSUE_NUMBER"
-        break
-    fi
-done
-
-if [ -n "$EXISTING_ISSUE_NUMBER" ]; then
-  curl \
-    -X PATCH \
-    -H "Authorization: token $GH_TOKEN" \
-    -H "Accept: application/vnd.github.v3+json" \
-    https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$EXISTING_ISSUE_NUMBER \
-    -d "$(create_issue_payload)"
-else
-  curl \
+curl \
     -X POST \
     -H "Authorization: token $GH_TOKEN" \
     -H "Accept: application/vnd.github.v3+json" \
     https://api.github.com/repos/$GITHUB_REPOSITORY/issues \
     -d "$(create_issue_payload)"
-fi
