bfb55d926470fda66eda48f6631a7e9b6c1757ae
Eugene Lositsky <evgenylositskyitmo@gmail.com>
2023-07-16 14:33:36 +0400

    fix: store update to test tags

--- src/store.ts
+++ src/store.ts
@@ -6,7 +6,7 @@ export interface ApplicationState {
   done: Record<number, boolean>;
 }
 
-const items = ["Позавтракать", "Сходить в магазин", "Покормить кота", "Почиллить", "Пофлексить"];
+const items = ["Позавтракать", "Сходить в магазин", "Покормить кота", "Почиллить"];
 
 const DEFAULT_STATE = { items, done: {}, text: "" };
 
