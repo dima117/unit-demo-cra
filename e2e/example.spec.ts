26fc4d448e6774d16e3c66811a086e8dd86b841b
neemkashu <sparjinkust@mail.ru>
2023-07-17 17:47:25 +0300

    test: change routing

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from "@playwright/test";
 
 test('по адресу /about открывается страница "о проекте"', async ({ page }) => {
-  await page.goto("/about");
+  await page.goto("./about");
 
   await expect(page.getByTestId("page-title")).toHaveText("About");
 });
 
 test("если добавить элемент, он появляется в списке", async ({ page }) => {
-  await page.goto("/");
+  await page.goto("./");
 
   await page.getByTestId("input-add").type("Сделать домашку");
   await page.getByTestId("button-add").click();
