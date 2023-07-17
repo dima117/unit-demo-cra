abec79eab299d1679d2813e1c3da7451c090aa3d
Eugene Lositsky <evgenylositskyitmo@gmail.com>
2023-07-17 04:18:38 +0400

    feat: task 6 in store

--- src/store.ts
+++ src/store.ts
@@ -6,7 +6,7 @@ export interface ApplicationState {
   done: Record<number, boolean>;
 }
 
-const items = ["Позавтракать", "Сходить в магазин", "Покормить кота", "Новая задача", "Задача номер пять"];
+const items = ["Позавтракать", "Сходить в магазин", "Покормить кота", "Новая задача", "Задача номер пять", "Задача номер 6"];
 
 const DEFAULT_STATE = { items, done: {}, text: "" };
 
