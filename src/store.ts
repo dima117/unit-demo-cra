f776dabf9197cac0fad7000e306387dca606661f
Eugene Lositsky <evgenylositskyitmo@gmail.com>
2023-07-17 04:25:50 +0400

    fix: deleted unused tasks from store

--- src/store.ts
+++ src/store.ts
@@ -6,7 +6,7 @@ export interface ApplicationState {
   done: Record<number, boolean>;
 }
 
-const items = ["Позавтракать", "Сходить в магазин", "Покормить кота", "Новая задача", "Задача номер пять", "Задача номер 6"];
+const items = ["Позавтракать", "Сходить в магазин", "Покормить кота", "Новая задача"];
 
 const DEFAULT_STATE = { items, done: {}, text: "" };
 
