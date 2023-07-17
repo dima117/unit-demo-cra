bd48b1f8be0cb7d1b80ec561b13c6d97bcf066eb
Ilya Sulkhanov <masta.pxty@gmail.com>
2023-07-17 19:49:32 +0700

    feat: break the test

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -3,17 +3,17 @@ import { test, expect } from "@playwright/test";
 test('по адресу /about открывается страница "о проекте"', async ({ page }) => {
   await page.goto("/about");
 
-  await expect(page.getByTestId("page-title")).toHaveText("About");
+  await expect(page.getByTestId("page-title")).toHaveText("AboutAwdada");
 });
 
 test("если добавить элемент, он появляется в списке", async ({ page }) => {
   await page.goto("/");
 
-  await page.getByTestId("input-add").type("Сделать домашку");
+  await page.getByTestId("input-add").type("Сделать домашкуawdawd");
   await page.getByTestId("button-add").click();
 
   const items = page.getByTestId("list-item");
   const allTexts = await items.allTextContents();
 
-  await expect(allTexts).toContain("Сделать домашку");
+  await expect(allTexts).toContain("Сделать домашкуAdAWD");
 });
