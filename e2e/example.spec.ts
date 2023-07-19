f182f0e9f25913220151f3d969581990521f1a76
ivan <shatowkln.ivan@yandex.ru>
2023-07-20 00:25:43 +0300

    feat: add new bug

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/test";
 test('по адресу /about открывается страница "о проекте"', async ({ page }) => {
   await page.goto("/about");
 
-  await expect(page.getByTestId("page-title")).toHaveText("About");
+  await expect(page.getByTestId("page-title")).toHaveText("AboutTest");
 });
 
 test("если добавить элемент, он появляется в списке", async ({ page }) => {
