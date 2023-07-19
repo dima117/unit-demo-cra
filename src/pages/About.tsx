3ffaa76b2cce11aca6d417f60fc5110db723bf84
maxim <fedaraumaxim@gmail.com>
2023-07-19 10:37:39 +0300

    feat: update about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -8,7 +8,7 @@ export const About: FC = () => {
         <title>About page</title>
       </Helmet>
       <h1 data-testid="page-title">About</h1>
-      <p>This is an example application.</p>
+      <p>This is an example application. New</p>
     </>
   );
 };
